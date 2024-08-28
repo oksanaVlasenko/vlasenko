9785ab1610b35ad79717206557be1c7ae5fc05ba
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2024-08-28 13:22:11 +0300

    fix: env files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV.BASE,
+  base: process.env.VITE_BASE_URL,
   
   plugins: [vue()],
   resolve: {
