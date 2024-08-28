9cca9a2b2cfbcbcb849d78c6627a34c227935cb8
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2024-08-28 22:19:35 +0300

    adding dist

---
+++ dist/assets/CV_Oksana_Vlasenko-DxRa8MfR.js
@@ -0,0 +1,1 @@
+const s="/vlasenko/assets/CV_Oksana_Vlasenko-Cv0ZRjmv.pdf";export{s as p};
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 const routes = [
   { 
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/vlasenko/' : '/'),
   routes,
 })
 
