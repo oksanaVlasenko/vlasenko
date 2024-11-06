dbcb88bea570907fdd0f3574df8d464837a0cbcd
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2024-11-06 16:54:45 +0200

    adding dist subtree

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { isScrolling } from '@/state/useScrollState';
 
 import Home from '@/components/Home.vue'
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from) {
     if (!isScrolling.value) {
